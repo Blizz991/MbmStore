0f76cdf9239dd9d629e0f376c50eb06d66890799
Blizz991 <martinhblizz@gmail.com>
2021-03-02 13:45:37 +0100

    Added Invoice and OrderItem classes

---
+++ Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; }
+        public Customer Customer { get; set; }
+        public List<OrderItem> OrderItems { get; } = new List<OrderItem>();
+
+        public Invoice() { }
+
+        public Invoice(int invoiceId, DateTime orderDate, Customer customer)
+        {
+
+        }
+
+        public void AddOrderItem(OrderItem orderItem)
+        {
+            OrderItems.Add(orderItem);
+        }
+    }
+}
