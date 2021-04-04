92f0dd5f9b9255c290b40f79b77021d0eb3b0a3e
Blizz991 <martinhblizz@gmail.com>
2021-04-04 11:01:24 +0200

    Added catalogue navigation by category

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using MbmStore.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.CurrentCategory = RouteData?.Values["category"];
+            return View(Repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x)
+            );
+        }
+    }
+}
