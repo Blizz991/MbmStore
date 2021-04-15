4d681595e9a32844423141474d712fb404a4293a
Blizz991 <martinhblizz@gmail.com>
2021-04-15 12:37:16 +0200

    Added new phone class

---
+++ Models/Phone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+        public int Number { get; set; }
+        public int CustomerId { get; set; }
+        public string PhoneType { get; set; }
+        public Customer Customer { get; set; }
+
+        public Phone(){}
+
+        public Phone(int phoneId, int number, int customerId, string phoneType)
+        {
+            PhoneId = phoneId;
+            Number = number;
+            CustomerId = customerId;
+            PhoneType = phoneType;
+        }
+    }
+}
